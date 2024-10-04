fe5aed35e3d8ae2c25e6d1196adf1665218db121
diegdar <diegdarch@hotmail.com>
2024-10-04 19:06:27 +0200

    ✨ feat(api): add routes for user and activity endpoints

---
+++ routes/api.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    ActivityController,
+    RegisterController,
+    UserController,
+};
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Rutas de registro y login
+Route::post('/register', [RegisterController::class, 'register'])->name('user.register');
+Route::post('/login', [RegisterController::class, 'login'])->name('login');
+
+
+Route::prefix('/appActivities')->group(function () {
+    // Rutas de usuarios
+    Route::prefix('/users')->group(function () {
+        Route::get('/{user}', [UserController::class, 'show'])->name('user.details');
+
+        Route::put('/{user}', [UserController::class, 'update'])->name('user.update');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('user.delete');
+
+        // Rutas de actividades relacionadas con el usuario
+        Route::post('/{user}/activities/{activity}', [ActivityController::class, 'joinActivity'])->name('user.activity.join');
+    });
+
+
+    // Rutas de actividades
+    Route::post('/activity', [ActivityController::class, 'store'])->name('activity.create');
+    Route::get('/activity/{activity}', [ActivityController::class, 'show'])->name('activity.details');
+
+    
+    // Importacion y exportacion actividades
+    Route::get('/export/activities', [ActivityController::class, 'exportActivities'])->name('activities.export');
+    Route::post('/import/activities', [ActivityController::class, 'importActivities'])->name('activities.import');
+
+});
+
+
+
