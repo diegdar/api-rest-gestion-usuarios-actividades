5b9d60c217ef0d89f42b0efde1826b45ec6c4c73
diegdar <diegdarch@hotmail.com>
2025-02-10 16:35:30 +0100

    feat(Exceptions): :sparkles: add custom exception handling and logging for various error types

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,14 @@
 
 declare(strict_types=1);
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,8 +19,42 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->reportable(function (NotFoundHttpException $e) {
+            Log::error('Recurso no encontrado: ' . $e->getMessage());
+        });
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json(['error' => 'Recurso no encontrado'], 404);
+        });
+
+        $exceptions->reportable(function (ValidationException $e) {
+            Log::error('Error de validación: ' . $e->getMessage());
+        });
+        $exceptions->renderable(function (ValidationException $e) {
+            return response()->json(['error' => 'Datos de entrada inválidos'], 422);
+        });
+
+        $exceptions->reportable(function (AuthenticationException $e) {
+            Log::error('Error de autenticación: ' . $e->getMessage());
+        });
+        $exceptions->renderable(function (AuthenticationException $e) {
+            return response()->json(['error' => 'No autenticado'], 401);
+        });
+
+
+        $exceptions->reportable(function (AuthorizationException $e) {
+            Log::error('Error de autorización: ' . $e->getMessage());
+        });
+        $exceptions->renderable(function (AuthorizationException $e) {
+            return response()->json(['error' => 'No autorizado'], 403);
+        });
+
+        $exceptions->reportable(function (Throwable $e) {
+            Log::error('Error interno del servidor: ' . $e->getMessage());
+        });
+        $exceptions->renderable(function (Throwable $e) {
+            return response()->json(['error' => 'Error interno del servidor'], 500);
+        });
+    })
+    ->create();
